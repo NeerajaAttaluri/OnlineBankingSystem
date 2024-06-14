31f72dbd7a45a3ad6ddc37efbfd1bbad0e7c89f1
NeerajaAttaluri <neerajaattaluri15@gmail.com>
2024-06-14 13:08:29 +0530

    Add files via upload

---
+++ dao/AccountantDAO.java
@@ -0,0 +1,27 @@
+package dao;
+import dao.bean.AccountantBean;
+import dao.bean.CustomerBean;
+import exception.AccountException;
+import exception.AccountantException;
+import exception.CustomerException;
+
+public interface AccountantDAO {
+	
+	public AccountantBean LoginAccountant(String username, String password)throws AccountantException;
+	
+	public int addCustomer(String cname,String cmail, String cpass, String cmob, String cadd) throws CustomerException;
+
+	public String addAccount(int cbal,int cid) throws AccountException;
+	
+	public String updateCustomer(int cACno,String cadd) throws CustomerException;
+
+	public  CustomerBean viewCustomer(String cACno) throws CustomerException;
+	
+	public int getCustomer(String cmail,String cmob) throws CustomerException;
+	
+	public CustomerBean viewAllCustomer() throws CustomerException;
+	
+	public String deleteAccount(int cACno) throws CustomerException;
+	
+	
+}
